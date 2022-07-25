14d5e073c94a1dd56b1dd7cd057ae16120ba14e9
Yener88 <106224876+Yener88@users.noreply.github.com>
2022-07-25 10:30:29 +0200

    25-07-22 1.1

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,18 +7,30 @@ import { StartscreenComponent } from './startscreen/startscreen.component';
 import { GameComponent } from './game/game.component';
 import { PlayerComponent } from './player/player.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MatButtonModule } from '@angular/material/button';
+import { MatIconModule } from '@angular/material/icon';
+import { DialogAddPlayerComponent } from './dialog-add-player/dialog-add-player.component';
+import { MatDialogModule } from '@angular/material/dialog';
+import { MatInputModule } from '@angular/material/input';
+import { FormsModule } from '@angular/forms';
 
 @NgModule({
   declarations: [
     AppComponent,
     StartscreenComponent,
     GameComponent,
-    PlayerComponent
+    PlayerComponent,
+    DialogAddPlayerComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
-    BrowserAnimationsModule
+    BrowserAnimationsModule,
+    MatButtonModule,
+    MatIconModule,
+    MatDialogModule,
+    MatInputModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
