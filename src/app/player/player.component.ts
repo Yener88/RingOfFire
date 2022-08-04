aa5e78146cd1de77696dc2ad8484bb36ad5b0c3d
Yener88 <106224876+Yener88@users.noreply.github.com>
2022-08-04 11:08:08 +0200

    04.08.22 1.1

--- src/app/player/player.component.ts
+++ src/app/player/player.component.ts
@@ -7,7 +7,7 @@ import { Component, Input, OnInit } from '@angular/core';
 })
 export class PlayerComponent implements OnInit {
   @Input() name;
-  @Input() image;
+  @Input() image = 'male1.png';
   @Input() playerActive: boolean = false;
 
 
